d441b9543426b4e1297da66f561155152ae00ec6
Estevam Galvão Albuquerque <estevamgalvao1@gmail.com>
2018-05-21 00:04:52 -0300

    prototype Q2

--- Question2.py
+++ Question2.py
@@ -2,26 +2,50 @@ import numpy as np
 import cv2
 from functions.rgb_ycbcr import RGB_YCbCr
 
-croppedAirplane = cv2.imread("cropped_plane.bmp")
-height, width, channels = croppedAirplane.shape
+print("Procurando...\n"*15)
 
-print(len(croppedAirplane))
-print(len(croppedAirplane[0]))
+def mse(imageA, imageB):
+    # the 'Mean Squared Error' between the two images is the
+    # sum of the squared difference between the two images;
+    # NOTE: the two images must have the same dimension
+    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+    err /= float(imageA.shape[0] * imageA.shape[1])
 
-RGB_YCbCr(croppedAirplane, 1)
+    # return the MSE, the lower the error, the more "similar"
+    # the two images are
+    return err
 
-pTable = {}
-maior = 0
-for i in range(height):
-    for j in range(width):
-        if croppedAirplane[i, j, 0] in pTable:
-            pTable[croppedAirplane[i, j, 0]] += 1
-            if pTable[croppedAirplane[i, j, 0]] > maior:
-                maior = pTable[croppedAirplane[i, j, 0]]
-                maiorAdress = croppedAirplane[i, j, 0]
-        else:
-            pTable[croppedAirplane[i, j, 0]] = 1
+# croppedAirplane = cv2.imread("cropped_plane.bmp")
+# height, width, channels = croppedAirplane.shape
+# RGB_YCbCr(croppedAirplane, 1)
 
-print(pTable)
-print(maior)
-print(maiorAdress)
+
+########################################################
+DenoisedImage = cv2.imread("./aux_images/FinalImage.bmp")
+RGB_YCbCr(DenoisedImage, 1)
+height, width, channels = DenoisedImage.shape
+
+croppedAirplane = DenoisedImage[488:613, 408:495, 0]
+heightCut, widthCut = croppedAirplane.shape
+cv2.imshow('cropped', croppedAirplane)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+for y in range(height - heightCut):
+    for x in range(width - widthCut):
+        if mse(DenoisedImage[y:y+heightCut, x:x+widthCut, 0], croppedAirplane) == 0:
+            print("Achei um avião!")
+            print("\t Intervalo de Coordenadas: [%d, %d][%d, %d]" %(y, y+heightCut, x, x+widthCut))
+
+# sum = float(np.sum(croppedAirplane[:, :, 0]))
+# print(sum)
+#
+# averageW = sum/(len(croppedAirplane) * len(croppedAirplane[0]))
+# if (averageW - int(averageW) > 0.5):
+#     averageW = int(averageW) + 1
+# else:
+#     averageW = int(averageW)
+# print(averageW)
+
+# Eu acredito q o resultado seja no canto superior esquerdo da template
+# X e y são o local por onde a qual ocorre a operação, e s e t são  as dimensões da template
